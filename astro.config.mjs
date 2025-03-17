df9aeb96e03a0ebe4a108d7fd7d1c81283c4f38c
Facundo Eguillor <eguillorfacundo@gmail.com>
2025-03-17 16:32:02 -0300

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://eguillor.github.io',
-    base: 'Espacio-Odontol-gico',
+    base: 'espacioOdn.github.io',
 });
