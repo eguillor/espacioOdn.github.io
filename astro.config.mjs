b99ba1e344ff65edd474cd05316822e31e5d3e62
Facundo Eguillor <eguillorfacundo@gmail.com>
2025-03-27 10:02:38 -0300

    Nuevos cambios v-4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://eguillor.github.io',
-    base: 'espacioOdn.github.io',
+    // base: 'espacioOdn.github.io',
 });
